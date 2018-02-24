ec1a36845a69a7db8a3c94b7dd5b0d1e449d16f5
Josef Pohořelý <josef.pohorely@embedit.cz>
2018-02-24 15:55:27 +0100

    SLN modification

--- src/init.cmd
+++ src/init.cmd
@@ -2,3 +2,7 @@ dotnet new sln -n CMenu
 dotnet new classlib -n CMenu
 dotnet new console -n CMenu.Console
 dotnet new xunit -n CMenu.Tests
+
+dotnet sln CMenu.sln add CMenu\CMenu.csproj
+dotnet sln CMenu.sln add CMenu.Console\CMenu.Console.csproj
+dotnet sln CMenu.sln add CMenu.Tests\CMenu.Tests.csproj
